28aeb05d54426f6e81dc0df2784a241e9c63ef30
kleinsimon <sklein8@googlemail.com>
2016-08-11 22:48:18 +0200

    added

---
+++ sensor.py
@@ -0,0 +1,41 @@
+#!/usr/local/bin/python3.5
+# -*- coding: utf-8 -*-
+import re, os, time, sys, config
+
+def readSensor(path):
+	value = 0
+	try:
+		f = open(path, "r")
+		line = f.readline()
+		if re.match(r"([0-9a-f]{2} ){9}: crc=[0-9a-f]{2} YES", line):
+			line = f.readline()
+			m = re.match(r"([0-9a-f]{2} ){9}t=([+-]?[0-9]+)", line)
+			if m:
+				value = (float(m.group(2)) / 1000.0)
+		f.close()
+	except IOError as e:
+		print (time.strftime("%x %X"), "Error reading", path, ": ", e)
+	return value
+
+def getSensors():
+	sensors=[]
+	slist=os.listdir(config.sensorPath)
+	for s in slist:
+		p=config.sensorPath+ "/" + s + "/w1_slave"
+		if os.path.exists(p):
+			sensors.append(p)
+	return sensors
+
+def getValues():
+	global sensors
+	res=[]
+	for s in sensors:
+		res.append(readSensor(s))
+	return res
+
+sensors=getSensors()
+	
+if __name__ == "__main__":
+	print (getValues())
+
+  
